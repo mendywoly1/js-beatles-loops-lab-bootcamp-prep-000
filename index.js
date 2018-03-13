cee02bb8fc3fa5f54fc73b8681e79e345f8fe35e
Mendy Wolosow1 <mendywoly+1@gmail.com>
2018-03-13 10:57:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function theBeatlesPlay(musicians, instruments) {
   var arr1 = []
   for(var i = 0; i > musicians.length ; i++) {
     arr1.push(musicians[i] +" plays " + instruments[i])
-    return arr1
   }
+  return arr1
 }
 
