d90ea2b65311427d6d8a0a60cab58ce6d4d1033a
Mendy Wolosow1 <mendywoly+1@gmail.com>
2018-03-13 10:55:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-function johnLennonFacts(array) {
-  
+console.log(musicians[0])
+
+function theBeatlesPlay(musicians, instruments) {
+  var arr1 = []
+  for(var i = 0; i > musicians.length ; i++) {
+    arr1.push(musicians[i] +" plays " + instruments[i])
+  }
+  return arr1
 }
+
